0995754405fe1c0fadca7db6393ab1aa36b96181
Chris Wright <cwright@talroo.com>
2018-09-10 08:16:14 -0500

    add files to minify static pages and remove code of examples.  Also turned off error handeling

--- index.php
+++ index.php
@@ -3,16 +3,22 @@
 <head>
 <title>Inner XML Parsing Tool Talroo</title>
   <meta charset="UTF-8">
+  <link rel="icon" href="static/img/five.png">
   <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+  <link rel="stylesheet" href="static/css/bootstrap.min.css">
   <!-- jQuery library -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+  <script src="static/js/jquery321min.js"></script>
   <!-- Latest compiled JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
+  <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script> -->
+  <script src="static/js/413bootstrapbundelmin.js"></script>
   <!-- AngularJS framework -->
-  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js"></script>
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script> -->
+  <script src="static/js/164angularmin.js"></script>
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js"></script> -->
+  <script src="static/js/169angularroutemin.js"></script>
   <script src="static/js/app.js"></script>
   <link rel="stylesheet" type="text/css" href="static/css/style.css" />
   <style>
@@ -29,11 +35,7 @@ input[type=text] {
   <div class="jumbotron">
     <h1> XML Tool
     </h1>
-    <!-- <p>Testurl: </br>
-    ftp://recruitics:sc1tiurcer@www2.jobs2careers.com/1051_JC.xml</br>
-    ftp://recruitics:sc1tiurcer@www2.jobs2careers.com/994_JC_PPA.xml</br>
-    https://exchangefeeds.s3.amazonaws.com/3f8752623cd8d9075ec95b01a021ab0d/feed.xml
-  </p> -->
+    <?php //echo "ftp://recruitics:sc1tiurcer@www2.jobs2careers.com/1051_JC.xml</br> ftp://recruitics:sc1tiurcer@www2.jobs2careers.com/994_JC_PPA.xml</br> https://exchangefeeds.s3.amazonaws.com/3f8752623cd8d9075ec95b01a021ab0d/feed.xml";?>
   </div>
 </header><p></p>
 <!-- Make bootstrap container -->
@@ -69,6 +71,7 @@ input[type=text] {
         </div>
       </form>
       <?php
+          error_reporting(0);
           $url = $_POST['url'];
           $values =  explode(",",$_POST['values']);
           // $clickcast = $_POST['clickcast'];
