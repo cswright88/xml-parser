4ecea4905931964baf2511453321ddfddc0b95aa
Chris Wright <cwright@talroo.com>
2018-09-25 15:39:21 -0500

    add function to limit the download of super large files to prevent issues

--- scripts/download.php
+++ scripts/download.php
@@ -10,13 +10,33 @@ class dlXML {
     public function getUrl(){
         return $this->url;
     }
+    public function retrieve_remote_file_size($file){
+        $ch = curl_init($file);
+    
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, TRUE);
+        curl_setopt($ch, CURLOPT_NOBODY, TRUE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    
+        $data = curl_exec($ch);
+        $size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+    
+        curl_close($ch);
+        return $size;
+    }
 
     public function download(){
+        set_time_limit(60);
         if (preg_match("/(.gz)$/",$this->url)){
             $this->url = "compress.zlib://" . $this->url;
         }
-        $xml = simplexml_load_file($this->url) or die("feed not loading");
-        return $xml;
+        if($this->retrieve_remote_file_size($this->url) < 100000000){
+            $xml = simplexml_load_file($this->url) or die("feed not loading");
+            return $xml;
+        }else {
+            die("feed too large - please get with someone who knows grep");
+        }
+
     }
 }
 // $u = "ftp://recruitics:sc1tiurcer@www2.jobs2careers.com/1038_JC.xml";
